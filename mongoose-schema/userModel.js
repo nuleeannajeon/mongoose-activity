0de602b7ab1e26b7c175fb0cf61dd2a203e9dd48
Anna Jeon <Anna@Annas-MacBook-Air.local>
2020-07-13 10:58:17 -0400

    schema activity

---
+++ mongoose-schema/userModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: 'Username is Required'
+  },
+
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+    validate: [({ length }) => length >= 6, "Longstring should be longer."]
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+  },
+
+  userCreated: {
+    type: Date,
+    default: Date.now
+  }
+
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
+// * In `userModel.js` add four attributes to your schema.
+
+//   * username: A string that will be be required, and also trimmed.
+
+//   * password: A string that will be required, trimmed, and at least 6 characters.
+
+//   * email: A string that must be a valid email address and unique in our collection.
+
+//   * userCreated: A date that will default to the current date.
